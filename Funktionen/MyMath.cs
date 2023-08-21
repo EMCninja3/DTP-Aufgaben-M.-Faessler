9f6a0fbcc607336ff0f3fb4c03605347bf7b86b2
EMCninja3 <emcninja333@gmail.com>
2023-08-21 15:06:17 +0200

    Aufgabe 1...3

---
+++ Funktionen/MyMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funktionen
+{
+    internal class MyMath
+    {
+        int a = 12, b = 16;
+        
+
+        public int Calc_kgV(int a, int b){
+            int result = (a * b) / Calc_ggT(a,b);
+            return result; 
+        }
+
+        public int Calc_ggT(int a, int b)
+        {
+            int z = 0;
+            while (b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+            return a;
+        }
+    }
+}
---
+++ Funktionen/Program.cs
@@ -0,0 +1,15 @@
+﻿using Funktionen;
+
+namespace Aufgabe_Funktionen
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            MyMath myMath = new MyMath();
+            int a = 6; int b = 8;
+            Console.WriteLine($"kgV von {a} un {b} = {myMath.Calc_kgV(a, b)}");
+            Console.WriteLine($"ggT von {a} un {b} = {myMath.Calc_ggT(a, b)}");
+        }
+    }
+}
