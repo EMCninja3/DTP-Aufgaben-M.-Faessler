cacaf8777913fffbf7bbfea778593e811f23444e
EMCninja3 <emcninja333@gmail.com>
2023-09-04 16:51:02 +0200

    Erstellung Aufgabe Portfolio Objektbasierte Programmierung

---
+++ PortfolioAufgabeGeoShapes/Kreis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioAufgabeGeoShapes
+{
+    public class Kreis
+    {
+        public Linie umfang;
+        public Kreis(Linie umfang) {
+            this.umfang = umfang;
+        }
+
+        public double BerechneUmfang()
+        {
+            return umfang.Laenge;
+        }
+
+        public double BerechneFlaeche()
+        {
+            double flaeche = 0;
+            double radius = umfang.Laenge / (2 * Math.PI);
+            flaeche = Math.Pow(radius, 2) * Math.PI;
+
+            return flaeche;
+        }
+    }
+}
---
+++ PortfolioAufgabeGeoShapes/Linie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioAufgabeGeoShapes
+{
+    public class Linie
+    {
+        public double Laenge { get; set; }
+    }
+}
